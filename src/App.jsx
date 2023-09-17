e749c62d518972bc3f2ff2507a402cf8de9ea550
Fabianormo <fabianormo@outlook.com>
2023-09-17 08:17:12 -0500

    feat: 🎸 Clase 01: install React + Vite + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <p className="bg-red-500 text-white p-4">
+      Este es un párrafo con fondo rojo y texto blanco.
+    </p>
+  );
+}
+
+export default App;
