c358cc1d8a092204728813bb0b29451f7fa8b65c
Fabianormo <fabianormo@outlook.com>
2023-09-18 19:09:51 -0500

    feat: 🎸 Clase 08: consumiendo API para pintar cards

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
 import Card from "../../components/card";
 import Layout from "../../components/layout";
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <Layout>
-      Home <Card />
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
     </Layout>
   );
 };
